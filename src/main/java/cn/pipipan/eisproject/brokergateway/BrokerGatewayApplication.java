217ae55f5af0f6d2f078769c90a3cdb53055b0aa
ssingularity <1755405701@qq.com>
2019-04-30 16:36:58 +0800

    init

---
+++ src/main/java/cn/pipipan/eisproject/brokergateway/BrokerGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.pipipan.eisproject.brokergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/pipipan/eisproject/brokergateway/BrokerGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.pipipan.eisproject.brokergateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrokerGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
