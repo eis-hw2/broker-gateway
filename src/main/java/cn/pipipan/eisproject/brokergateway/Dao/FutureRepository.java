dd8924aa5920c28eeecbda1d9eb992467ed2bd19
ssingularity <1755405701@qq.com>
2019-05-06 15:14:20 +0800

    seperate future

--- src/main/java/cn/pipipan/eisproject/brokergateway/Dao/FutureRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.pipipan.eisproject.brokergateway.Dao;
-
-import cn.pipipan.eisproject.brokergateway.Domain.Future;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FutureRepository extends MongoRepository<Future, String> {
-}
--- src/main/java/cn/pipipan/eisproject/brokergateway/Dao/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.pipipan.eisproject.brokergateway.Dao;
-
-import cn.pipipan.eisproject.brokergateway.Domain.Item;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ItemRepository extends MongoRepository<Item, String> {
-}
