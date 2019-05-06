267697e180b45bdc618906aa4de140ab47228efa
ssingularity <1755405701@qq.com>
2019-05-06 14:54:34 +0800

    add axon

--- src/main/java/cn/pipipan/eisproject/brokergateway/Controller/TestController.java
+++ src/main/java/cn/pipipan/eisproject/brokergateway/Controller/TestController.java
@@ -1,18 +1,9 @@
 package cn.pipipan.eisproject.brokergateway.Controller;
 
-import cn.pipipan.eisproject.brokergateway.Domain.Order;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 public class TestController {
@@ -21,6 +12,11 @@ public class TestController {
     @GetMapping("/test")
     public void test(){
         rabbitTemplate.convertAndSend("exchange", "buyer", 123);
+    }
+
+    @GetMapping("/refresh")
+    public void refresh(){
+        //TODO 清空内存
     }
 
 }
